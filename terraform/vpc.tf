8a9d1d60fbd24927c3a7dbc244d1ab2c7c6f8dff
karthik Duggana <d.karthikyadav94@gmail.com>
2025-05-02 10:54:59 +0530

    final

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -41,6 +41,24 @@ resource "aws_security_group" "ecs_tasks" {
 }
 
 # networking.tf
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "${var.app_name}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.app_name}-public-subnet-${count.index}"
+  }
+}
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags = {
